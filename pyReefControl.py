b27a981cc6315bb3a9f5c4271fb8239adf1e2088
Ed Bortoni <ed@bortoni.com>
2016-06-28 17:06:22 -0400

    fix feeding and passing data between threads.

--- pyReefControl.py
+++ pyReefControl.py
@@ -1,19 +1,16 @@
 import json
 import os
 import time
+import threading
 from datetime import datetime
 from pprint import pprint
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import multiprocessing
+from multiprocessing import Value
+from copy import deepcopy
 
 
 
-########### GLOBALS #########
-defaults = {}
-powermap = {}
-schedule = {}
-overrides_list = {}
-temperature = 78.0
 
 ############## WEB APP
 app = Flask('PyReefControl')
@@ -24,27 +21,26 @@ def main_page():
 	feed_text = "Stop Feeding"
 	now = datetime.now()
 	timeString = now.strftime("%Y-%m-%d %H:%M")
-	feeding = get_current_feed_state();
-	if feeding == "NO":
+#	feeding = get_current_feed_state();
+	if feeding.value == 0:
 		feed_text = "Feed"
 	templateData = {
       'title' : 'pyReefControl',
       'time': timeString,
-      'temp': temperature,
+      'temp': temperature.value,
       'feed': feed_text
       }
 	return render_template('main.html', **templateData)
 
 @app.route("/feed")
 def feed_request():
-	feeding = get_current_feed_state();
-	if feeding == "NO":
-		set_feed_override("YES")
+	if feeding.value == 0:
+		feeding.value = 1
 	else:
-		set_feed_override("NO")
-	return main_page()
+		feeding.value = 0
+	return redirect(url_for('main_page'))
 
-def webapp_main():
+def webapp_main(feeding, temperature):
 	name = multiprocessing.current_process().name
 	print(name, 'Starting')
 	app.run(host='0.0.0.0', port=80, debug=True, use_reloader=False)
@@ -59,7 +55,7 @@ def control_power(name, state):
 	password = defaults["SWITCH_PASS"]
 	ip = defaults["SWITCH_IP"]
 	cmd = "curl http://" + user +':'+ password + '@' + ip + "/outlet?" + powermap[name]+'='+state
-	print(cmd)
+#	print(cmd)
 #	os.system(cmd)
 #	curl http://admin:admin01@10.10.10.10/outlet?1=OFF
 	return
@@ -80,25 +76,40 @@ def find_active_event(event_list):
 			retval = i
 	return retval
 
-def find_active_override(overrides):
+def find_active_override():
+	global overrides_list
 	retval = None
 
+	print("In find_active_override.")
+	pprint(overrides_list)
+
 	# currently supporting only the first active override
-	for i in range(len(overrides["overrides"])):
-		active = overrides["overrides"][i]["event"][0]["active"]
+	for i in range(len(overrides_list["overrides"])):
+		active = overrides_list["overrides"][i]["event"][0]["active"]
 		if active == "YES":
 			print("Found active override:", i)
 			return i
 	return retval
 
 def set_feed_override(state):
+	global overrides_list
 	# currently supporting only the first active override
 	for i in range(len(overrides_list["overrides"])):
 		active = overrides_list["overrides"][i]["event"][0]["active"]
 		name = overrides_list["overrides"][i]["event"][0]["name"]
 		if name == "feed":
 			overrides_list["overrides"][i]["event"][0]["active"] = state
+			if state == "NO" and t.is_alive() == True:
+				print("Canceling Feed Timer")
+				t.cancel()
+			elif state == "YES" and t.is_alive() == True:
+				print("Already FEEDING")
+
+			elif state == "YES" and t.is_alive() == False:
+				print("STARTING Feed Timer")
+				t.start()
 			print("Set feed override to:", state)
+	pprint(overrides_list)
 	return
 
 def get_current_feed_state():
@@ -112,19 +123,20 @@ def get_current_feed_state():
 	return "NO"
 
 
-def implement_event(event, overrides):
-	event_copy = event
-	active_override = find_active_override(overrides)
-	event_count = len(event["event"][0]["power_control"])
+def implement_event(event):
+	global overrides_list
 
-	pprint(event)
+	event_copy = deepcopy(event)
+	active_override = find_active_override()
+	event_count = len(event_copy["event"][0]["power_control"])
+
 	if active_override != None:
-		print("Active override:",overrides["overrides"][active_override]["event"][0]["name"])
-		count = len(overrides["overrides"][0]["event"][active_override]["power_control"])
+		print("Active override:", overrides_list["overrides"][active_override]["event"][0]["name"])
+		count = len(overrides_list["overrides"][0]["event"][active_override]["power_control"])
 		print("Overriding", count, "items")
 		for i in range(count):
-			name = overrides["overrides"][0]["event"][0]["power_control"][i]["name"]
-			state = overrides["overrides"][0]["event"][0]["power_control"][i]["state"]
+			name = overrides_list["overrides"][0]["event"][0]["power_control"][i]["name"]
+			state = overrides_list["overrides"][0]["event"][0]["power_control"][i]["state"]
 			print("Overriding", name, "to", state)
 			for j in range(event_count):
 				if name == event_copy["event"][0]["power_control"][j]["name"]:
@@ -132,29 +144,39 @@ def implement_event(event, overrides):
 
 	pprint(event_copy)
 
-	print("range is:", range(event_count))
 	for k in range(event_count):
 		control_power(event_copy["event"][0]["power_control"][k]["name"], event_copy["event"][0]["power_control"][k]["state"])
 		time.sleep(2)
 	return
 
+def feed_timer():
+	print("FEED TIMER EXPIRED")
+	return
+
 ################## SENSORS ######################
 def sensor_main():
-    name = multiprocessing.current_process().name
-    print(name, 'Starting')
-    defaults["SENSOR_INTERVAL"]
-    while True:
-    	print(name, 'sleeping', 60*int(defaults["SENSOR_INTERVAL"]))
-    	time.sleep(60*int(defaults["SENSOR_INTERVAL"]))
-    print(name, 'Exiting')
-
+	global defaults
+	name = multiprocessing.current_process().name
+	print(name, 'Starting')
+	defaults["SENSOR_INTERVAL"]
+	while True:
+		print(name, 'sleeping', 60*int(defaults["SENSOR_INTERVAL"]))
+		time.sleep(60*int(defaults["SENSOR_INTERVAL"]))
+	print(name, 'Exiting')
 
 
 
 ################ MAIN ###################
-
 if __name__ == "__main__":
-	# read defaults config
+	feeding = Value('i', 0) # 0 not feeding 1 feeding
+	feed_timer_started = False
+	temperature = Value('d', 78.0)
+	defaults = {}
+	powermap = {}
+	schedule = {}
+	overrides_list = {}
+
+# read defaults config
 
 	with open("./cfg/defaults.cfg") as f:
 		for line in f:
@@ -175,19 +197,33 @@ if __name__ == "__main__":
 		overrides_list = json.load(data_file)
 
 	sensor_thread = multiprocessing.Process(name='sensor_main', target=sensor_main)
-	webapp_thread = multiprocessing.Process(name='webapp_main', target=webapp_main)
+	webapp_thread = multiprocessing.Process(name='webapp_main', target=webapp_main, args=(feeding, temperature))
+	t = threading.Timer(60*int(defaults["FEED_DURATION"]), feed_timer)
 	sensor_thread.start()
 	webapp_thread.start()
 
 
-	# TODO wait for time!
+	# TODO wait for system time!
 
 	while True:
+		print("Feeding Value is", feeding.value)
+
+		if feeding.value == 1 and feed_timer_started == False :
+			set_feed_override("YES")
+			feed_timer_started = True
+
 		active_event = find_active_event(schedule["schedule"])
 		if active_event != None:
 			print("Index of active event:", active_event)
-			implement_event(schedule["schedule"][active_event], overrides_list);
+			implement_event(schedule["schedule"][active_event]);
 		else:
 			print("NO ACTIVE EVENT!!")
-		print("Main thread", 'sleeping', 60*int(defaults["EVENT_CHECK_INTERVAL"])+7)
+		print("Main thread", 'sleeping', 60*int(defaults["EVENT_CHECK_INTERVAL"]))
 		time.sleep(60*int(defaults["EVENT_CHECK_INTERVAL"]))
+
+		#if the timer is off and we were feeding, turn off feeding
+		if t.is_alive() == False and feed_timer_started == True:
+			print("TURNING OFF feeding in MAIN")
+			feeding.value = 0
+			set_feed_override("NO")
+			feed_timer_started = False
